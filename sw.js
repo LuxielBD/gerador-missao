2f74d2f42fc88ece4ce0f3977d8df38b5919f234
LuxielBD <l.d.montenegro.2015@gmail.com>
2026-01-04 19:55:49 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,9 @@
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('ordo-pwa-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ]);
-    })
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
+
+self.addEventListener('fetch', () => {});
